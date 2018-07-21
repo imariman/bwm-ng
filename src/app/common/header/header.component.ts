1130d8767a78802800cdc59591b881ccb9e29870
imariman <imariman@users.noreply.github.com>
2018-07-21 03:25:48 +0300

    Adds header components and scss styles

---
+++ src/app/common/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bwmheader',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+
+export class HeaderComponent {
+
+}
